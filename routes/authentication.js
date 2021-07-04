15ab489d1d7eeda993b3d84e74aaba8a65dc3278
souptik4572 <souptiksarkar4572@gmail.com>
2021-07-04 23:31:41 +0530

    completed authentication routes

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,11 +1,61 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-router.post('/register', (req, res) => {
-	res.send('Registration takes place here');
+router.post('/register', async (req, res) => {
+	const { name, email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (user) {
+		return res.status(404).json({
+			success: false,
+			error: 'Email already exists',
+		});
+	}
+	const salt = await bcrypt.genSalt(Number(process.env.BCRYPT_SALT));
+	const hashedPassword = await bcrypt.hash(password, salt);
+	try {
+		const user = await User.create({
+			name,
+			email,
+			password: hashedPassword,
+		});
+		return res.status(201).json({
+			success: true,
+			user,
+		});
+	} catch (error) {
+		return res.status(404).json({
+			success: false,
+			error: error.message,
+		});
+	}
 });
 
-router.post('/login', (req, res) => {
-	res.send('Logging in takes place here');
+router.post('/login', async (req, res) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		return res.status(404).json({
+			success: false,
+			error: 'User does not exist',
+		});
+	}
+	const isPasswordValid = await bcrypt.compare(password, user.password);
+	if (!isPasswordValid) {
+		return res.status(404).json({
+			success: false,
+			error: 'Password is incorrect',
+		});
+	}
+	const token = jwt.sign({ ...user }, process.env.ACCESS_SECRET_TOKEN, {
+		expiresIn: 3600,
+	});
+	return res.status(201).json({
+		success: true,
+		token,
+		userType: user.userType,
+	});
 });
 
 module.exports = router;
