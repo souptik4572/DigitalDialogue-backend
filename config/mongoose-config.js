7089ba1e83ee675b34d0074e65a8eb3c768010cc
Souptik Sarkar <souptiksarkar4572@gmail.com>
2025-08-01 22:56:48 +0530

    add sample env file with details

--- config/mongoose-config.js
+++ config/mongoose-config.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
 const mongoose = require('mongoose');
-const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/Sharodified';
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/Digital-Dialogue';
 
 const configureMongoose = () => {
 	mongoose.connect(
